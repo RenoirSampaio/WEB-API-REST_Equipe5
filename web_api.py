3f5800b5751bfb74683b7bc74f6c54a692ae0e25
brenocg <brenocg@alu.ufc.br>
2020-08-26 20:49:44 -0300

    primeiras rotas

--- web_api.py
+++ web_api.py
@@ -3,12 +3,49 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import json 
+
+
+df1 = pd.concat(pd.read_excel('indicadoressegurancapublicamunicmar20.xlsx', sheet_name=None), ignore_index=True)
+df2 = pd.read_excel('indicadoressegurancapublicaufmar20_ocorrencias.xlsx')
+df3 = pd.read_excel('indicadoressegurancapublicaufmar20_vítimas.xlsx')
+
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET'])
 def hello():
   return 'Hello World!'
+
+
+
+# GET ocorrências totais por UF
+# /api/ocorr/total/<UF:name>
+# Aqui ta retornando uma Series
+# Um tipo de dado do pandas
+@app.route('/ocorr/total/<UF>', methods=['GET'])
+def getOcorrTotalByUf(UF):
+  # if 'UF' in request.args:
+  #     UF = request.args['UF']
+  # else:
+  #     return "Error: Especifique o UF da consulta"
+  df2_prov = df2.loc[df2['UF'] == UF]
+  res = df2_prov.groupby("UF")["Ocorrências"].sum().to_json()
+  return res
+
+
+
+# # /api/ocorr/crim/tipos
+# Aqui ta retornando um array de strings.
+@app.route('/ocorr/crim/tipos', methods=['GET'])
+def getTiposCrimes():
+  res = np.unique(df2['Tipo Crime'])
+  a = res.tolist()
+  return json.dumps(a)
+
+
+
 
 if __name__ == '__main__':
   app.run()
