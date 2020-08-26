fa6a9e6e8e2862f4d68db0df9b829f48a089f3a7
Renoir Sampaio <renoirfla@gmail.com>
2020-08-26 15:14:29 -0300

    Possíveis rotas

--- web_api.py
+++ web_api.py
@@ -1,4 +1,8 @@
 from flask import Flask, make_response, jsonify, request
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,3 +12,13 @@ def hello():
 
 if __name__ == '__main__':
   app.run()
+
+# Possíveis rotas:
+# @app.route('/api/ocorr/total/<UF:name>', methods=['GET'])
+# @app.route('/api/vit/total/<UF:name>', methods=['GET'])
+# @app.route('/api/vit/<Município:name>', methods=['GET'])
+# @app.route('/api/ocorr/<Tipo Crime:name>', methods=['GET'])
+# @app.route('/api/vit/br/<Tipo Crime:name>', methods=['GET'])
+# @app.route('/api/vit/epoc/<Ano:name>', methods=['GET'])
+# @app.route('/api/ocorr/crim/tipos', methods=['GET'])
+# @app.route('/api/vit/crim/tipos', methods=['GET'])
